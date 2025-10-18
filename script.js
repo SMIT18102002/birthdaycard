a0005403b5160525213a3c5febb4435d97734529
SMIT18102002 <patelskumar2002@gmail.com>
2025-10-18 14:19:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ayu" && b=="2005")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
